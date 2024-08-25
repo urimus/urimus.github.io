9ab1448ff2f6edeed51788297f7dda0b50f556a8
Urmas Repinski <urrimus@hotmail.com>
2024-08-25 05:33:39 +0300

    some file type detection for upload removed 2

--- scripts/prev/store.js
+++ scripts/prev/store.js
@@ -121,4 +121,67 @@ function processEmptyFeed(type, source, lang, feedXML, turn) {
 
 }
 
-*/
+
+function isSignatureMatch(bytes, sig) {
+
+	if (Array.isArray(sig)) {
+		for (let i = 0; i < sig.length; i++) {
+			if (bytes[i] != sig[i]) return false;
+		}
+		return true;
+	}
+
+	if (typeof sig === "string") {
+		for (let i = 0; i < sig.length; i++) {
+			if (bytes[i] != sig.charCodeAt(i).toString(16)) return false;
+		}
+		return true;
+	}
+
+	return false;
+}
+
+
+//	get file type using magic numbers - more accurate
+//	Magic numbers - https://gist.github.com/leommoore/f9e57ba2aa4bf197ebc5
+
+	var fileReader = new FileReader();
+	fileReader.onload = function(event) {
+		arrayBuffer = event.target.result;
+		arr1 = new Uint8Array(arrayBuffer );
+		var bytes = [];
+		for (let i = 0; i < 12; i++) {
+			bytes[i]=arr1[i].toString(16);
+		}
+
+//console.log(bytes);
+
+
+
+		isImage=0;
+		imagetype="";
+		if (isSignatureMatch(bytes, "BM")) {isImage=1; imagetype="bmp";}
+		if (isSignatureMatch(bytes, "SIMPLE")) {isImage=1; imagetype="fits";}
+		if (isSignatureMatch(bytes, "GIF8")) {isImage=1; imagetype="gif";}
+		if (isSignatureMatch(bytes, "GKSM")) {isImage=1; imagetype="gks";}
+		if (isSignatureMatch(bytes, ["01", "da"])) {isImage=1; imagetype="rgb";}
+		if (isSignatureMatch(bytes, ["f1", "00", "40", "bb"])) {isImage=1; imagetype="itc";}
+		if (isSignatureMatch(bytes, ["ff", "d8", "ff", "e0"])) {isImage=1; imagetype="jpg";}
+		if (isSignatureMatch(bytes, "IIN1")) {isImage=1; imagetype="nif";}
+		if (isSignatureMatch(bytes, "VIEW")) {isImage=1; imagetype="pm";}
+		if (isSignatureMatch(bytes, ["89", "50", "4e", "47"])) {isImage=1; imagetype="png";}
+		if (isSignatureMatch(bytes, "%!")) {isImage=1; imagetype="[e]ps";}
+		if (isSignatureMatch(bytes, ["59", "a6", "6a", "95"])) {isImage=1; imagetype="ras";}
+		if (isSignatureMatch(bytes, ["4d", "4d", "00", "2a"])) {isImage=1; imagetype="tif";}
+		if (isSignatureMatch(bytes, ["49", "49", "2a", "00"])) {isImage=1; imagetype="tif";}
+		if (isSignatureMatch(bytes, "gimp xcf")) {isImage=1; imagetype="xcf";}
+		if (isSignatureMatch(bytes, "#FIG")) {isImage=1; imagetype="fig";}
+		if (isSignatureMatch(bytes, "/* XPM */")) {isImage=1; imagetype="xpm";}
+
+		if (isSignatureMatch(bytes, "RIFF")) {isImage=1; imagetype="webp";}
+		if (isSignatureMatch(bytes, "WEBP")) {isImage=1; imagetype="webp";}
+		if (isSignatureMatch(bytes, ["0", "0", "0", "1c", "66", "74", "79", "70", "61", "76", "69", "66"])) {isImage=1; imagetype="avif";}
+		if (isSignatureMatch(bytes, ["0", "0", "0", "20", "66", "74", "79", "70", "61", "76", "69", "66"])) {isImage=1; imagetype="avif";}
+
+	};
+	fileReader.readAsArrayBuffer(file.slice(0,12));
