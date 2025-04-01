0eb29caace0fb35e2602854f1f5e4d12fc293a05
Urmas Repinski <urrimus@hotmail.com>
2025-04-01 10:56:37 +0300

    Update processSearchAndReplace.js

--- scripts/processSearchAndReplace.js
+++ scripts/processSearchAndReplace.js
@@ -1769,7 +1769,7 @@ function logout(lang) {
 	}
 	xhr.onreadystatechange = function(){
         	if (this.readyState==4 && this.status==200) {
-			document.getElementById("scrollDiv").setAttribute("style", "height:"+menuHeight+"px;width: 718px; overflow:auto;");
+			document.getElementById("scrollDiv").setAttribute("style", "height:"+menuHeight+"px;width: 719px; overflow:auto;");
 //			console.log(this.responseText);
 //			alert(this.responseText);
 			window.location.href='html_editor_'+lang+'.html?pattern='+ encodeURIComponent(getParameterByName('pattern'))+'&i='+ encodeURIComponent(getParameterByName('i'));
@@ -2439,7 +2439,7 @@ function processSearchAndReplace(lang) {
 	isAndroid = ua.indexOf("android") && ua.indexOf("mobile") > -1; 
 	if (isAndroid) {
 		$("#error_message").text("HTML Editor is not supported for Android OS.");
-		document.getElementById("scrollDiv").setAttribute("style", "height:"+menuHeight+"px;width: 718px; overflow:auto;");
+		document.getElementById("scrollDiv").setAttribute("style", "height:"+menuHeight+"px;width: 719px; overflow:auto;");
 		return true; 
 	}
 
@@ -2461,7 +2461,7 @@ function processSearchAndReplace(lang) {
 			var respNoBOM=removeBom(this.responseText);
 			if (respNoBOM.substring(0, 2) == "<?") {
 				$("#error_message").text("PHP is not supported, HTML Editor is not functioning.");
-				document.getElementById("scrollDiv").setAttribute("style", "height:"+menuHeight+"px;width: 718px; overflow:auto;");
+				document.getElementById("scrollDiv").setAttribute("style", "height:"+menuHeight+"px;width: 719px; overflow:auto;");
 				return;
 			};
 
@@ -2493,7 +2493,7 @@ function processSearchAndReplace(lang) {
 					$("#error_message_with_logout_row").hide();
 					$("#error_message").text(err1);
 					$("#error_message_row").show();
-					document.getElementById("scrollDiv").setAttribute("style", "height:"+menuHeight+"px;width: 718px; overflow:auto;");
+					document.getElementById("scrollDiv").setAttribute("style", "height:"+menuHeight+"px;width: 719px; overflow:auto;");
 					return false;
 				}
 				
@@ -2516,11 +2516,11 @@ function processSearchAndReplace(lang) {
 							$("#error_message_with_logout_row").hide();
 							$("#error_message").text(err2);
 							$("#error_message_row").show();
-							document.getElementById("scrollDiv").setAttribute("style", "height:"+menuHeight+"px;width: 718px; overflow:auto;");
+							document.getElementById("scrollDiv").setAttribute("style", "height:"+menuHeight+"px;width: 719px; overflow:auto;");
 							return false;
 						}
 						scrollDivHeight=calcScrollDivHeightMax();
-						document.getElementById("scrollDiv").setAttribute("style", "height:"+scrollDivHeight+"px;width: 718px; overflow:auto;");
+						document.getElementById("scrollDiv").setAttribute("style", "height:"+scrollDivHeight+"px;width: 719px; overflow:auto;");
 						processSearch(lang);
 					}
 				};
@@ -2530,7 +2530,7 @@ function processSearchAndReplace(lang) {
 
 			} else {
 				scrollDivHeight=calcScrollDivHeightMax();
-				document.getElementById("scrollDiv").setAttribute("style", "height:"+scrollDivHeight+"px;width: 718px; overflow:auto;");
+				document.getElementById("scrollDiv").setAttribute("style", "height:"+scrollDivHeight+"px;width: 719px; overflow:auto;");
 				processSearch(lang);
 			}
         	}
