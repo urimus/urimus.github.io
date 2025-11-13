ed446d814d37aab63ace0756b46ef709439638d2
Urmas Repinski <urimus@yahoo.com>
2025-11-13 15:39:45 +0200

    sitemap generation 2

--- scripts/php/generate_sitemap.php
+++ scripts/php/generate_sitemap.php
@@ -7,26 +7,26 @@ $files = scandir($dir);
 
 $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>');
 
+$url = $xml->addChild('url');
+$url->addChild('loc', $baseUrl);
+$url->addChild('lastmod', date('Y-m-d'));
+
 foreach ($files as $file) {
-	if ($file === '.' || $file === '..' || $file === $sitemapFile) continue;
-	if (is_file($dir . '/' . $file) && strtolower(pathinfo($file, PATHINFO_EXTENSION)) === 'html') {
-		$url = $xml->addChild('url');
-		$url->addChild('loc', $baseUrl . $file);
-		$url->addChild('lastmod', date('Y-m-d', filemtime($dir . '/' . $file)));
-	}
+    if ($file === '.' || $file === '..' || $file === $sitemapFile) continue;
+    if (is_file($dir . '/' . $file) && strtolower(pathinfo($file, PATHINFO_EXTENSION)) === 'html') {
+        $url = $xml->addChild('url');
+        $url->addChild('loc', $baseUrl . $file);
+        $url->addChild('lastmod', date('Y-m-d', filemtime($dir . '/' . $file)));
+    }
 }
 
 $dom = new DOMDocument('1.0', 'UTF-8');
 $dom->preserveWhiteSpace = false;
 $dom->formatOutput = true;
 $dom->loadXML($xml->asXML());
-$xmlString = $dom->saveXML();
 
-// Приводим отступы к табуляции и добавляем переносы строк
-$xmlString = preg_replace('/^(\s+)/m', str_repeat("\t", 1), $xmlString);
-$xmlString = str_replace(array("\r\n", "\r"), "\n", $xmlString);
+file_put_contents($dir . '/' . $sitemapFile, $dom->saveXML());
 
-file_put_contents($dir . '/' . $sitemapFile, $xmlString);
-
-echo "Sitemap сгенерирован с читаемыми отступами и переносами строк!";
+echo $dom->saveXML();
+echo "\nSitemap сгенерирован!";
 ?>
