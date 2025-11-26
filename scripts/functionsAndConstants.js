83d0bdea9a2c287ecc5ba32c4763f79bb75d52c0
Urmas Repinski <urimus@yahoo.com>
2025-11-26 04:39:52 +0200

    getParameterByName returned to functionsAndConstants back

--- scripts/functionsAndConstants.js
+++ scripts/functionsAndConstants.js
@@ -1,5 +1,15 @@
 ﻿"use strict";
 
+function getParameterByName(name, url) {
+	if (!url) var url = window.location.href;
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+		results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 function mouseOut(tabType, feedTypeL, col) {
 	if (typeof col === 'undefined') var col = "red"; 
 	if (feedTypeL==tabType) {
