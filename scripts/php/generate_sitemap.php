63b268cd44d9fe68e60af6813c912c50184938aa
Urmas Repinski <urimus@yahoo.com>
2025-11-30 18:31:04 +0200

    Update generate_sitemap.php

--- scripts/php/generate_sitemap.php
+++ scripts/php/generate_sitemap.php
@@ -3,6 +3,12 @@ $dir = __DIR__;
 $sitemapFile = 'sitemap.xml';
 $baseUrl = 'https://urimus.wasmer.app/';
 
+$exclude = [
+	'index.html',
+	'test.html',
+	'google5577016dee36085f.html'
+];
+
 $files = scandir($dir);
 
 $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>');
@@ -13,6 +19,8 @@ $url->addChild('lastmod', date('Y-m-d'));
 
 foreach ($files as $file) {
 	if ($file === '.' || $file === '..' || $file === $sitemapFile) continue;
+	if (in_array($file, $exclude)) continue;
+
 	if (is_file($dir . '/' . $file) && strtolower(pathinfo($file, PATHINFO_EXTENSION)) === 'html') {
 		$url = $xml->addChild('url');
 		$url->addChild('loc', $baseUrl . $file);
@@ -27,7 +35,5 @@ $dom->loadXML($xml->asXML());
 
 file_put_contents($dir . '/' . $sitemapFile, $dom->saveXML());
 
-echo "Sitemap сгенерирован!\n";
-echo $dom->saveXML();
-
+echo '<script>alert("Sitemap сгенерирован - ' . $dir . '/' . $sitemapFile . '");</script>';
 ?>
