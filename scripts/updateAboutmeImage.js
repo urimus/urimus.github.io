9fc57bef1cbf7411b48012dbca37a64d957fdc13
Urmas Repinski <urimus@yahoo.com>
2026-02-01 12:39:38 +0200

    onkeydown for aboutMeImage corr

--- scripts/updateAboutmeImage.js
+++ scripts/updateAboutmeImage.js
@@ -166,6 +166,7 @@ function updateAboutMeImage2(lang, result, random) {
 	imageA.setAttribute('href', item.link);
 	imageA.setAttribute('class', 'standardb_blue');
 	imageA.setAttribute('target', '_blank');
+	imageA.setAttribute('onkeydown', "if(event.key==='Enter' || event.key===' ') { event.preventDefault(); this.click(); this.onmouseleave();}");
 	imageA.innerText = "Image #" + (i + 1);
 	Div.appendChild(imageA);
 	Div.innerHTML = Div.innerHTML + ".";
@@ -179,6 +180,7 @@ function updateAboutMeImage2(lang, result, random) {
 		var extensionA = document.createElement('a');
 		extensionA.setAttribute('href', "javascript:void(0);");
 		extensionA.setAttribute('class', 'standardb_blue');
+		extensionA.setAttribute('onkeydown', "if(event.key==='Enter' || event.key===' ') { event.preventDefault(); this.click(); this.onmouseleave();}");
 		extensionA.onclick = function () {
 			if (this.innerHTML == "[▼]") {
 				summarySpan.innerHTML = "&nbsp;" + item.content;
