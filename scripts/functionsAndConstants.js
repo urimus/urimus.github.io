4d76d2fca31b0c53b9c8db671c846c4d4fbc9374
Urmas Repinski <urimus@yahoo.com>
2026-02-15 05:55:05 +0200

    some html parameters work corr

--- scripts/functionsAndConstants.js
+++ scripts/functionsAndConstants.js
@@ -1,12 +1,7 @@
 ﻿"use strict";
 
-function getParameterByName(name, url = window.location.href) {
-	name = name.replace(/[\[\]]/g, "\\$&");
-	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-		results = regex.exec(url);
-	if (!results) return null;
-	if (!results[2]) return '';
-	return decodeURIComponent(results[2].replace(/\+/g, " "));
+function getParameterByName(name) {
+	return new URLSearchParams(window.location.search).get(name);
 }
 
 function mouseOutTab(tabType, feedTypeL, col = "red") {
