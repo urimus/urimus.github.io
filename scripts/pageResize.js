6b4593c3ded1fc3209014a66f9abdc68dd22679a
Urmas Repinski <urimus@yahoo.com>
2026-02-25 00:57:48 +0200

    Update pageResize.js

--- scripts/pageResize.js
+++ scripts/pageResize.js
@@ -220,7 +220,7 @@ function enableKeyboardScroll(scrollDiv) {
 
 	scrollDiv.style.scrollSnapType = 'x mandatory';
 
-	const step = 474;
+	const step = 450;
 	const step2 = () => scrollDiv.clientHeight;
 	var stepRepeat = 0;
 
