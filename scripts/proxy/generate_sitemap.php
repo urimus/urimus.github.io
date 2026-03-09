9021d4b6eaa4657e65f8940db3230678ae13a0b0
Urmas Repinski <urimus@yahoo.com>
2026-03-09 13:18:48 +0200

    generate sitemap mod

--- scripts/proxy/generate_sitemap.php
+++ scripts/proxy/generate_sitemap.php
@@ -1,91 +1,53 @@
 <?php
-$dir = __DIR__;
-$sitemapFile = 'sitemap.xml';
-$allFile = 'all.html';
-$baseUrl = 'https://urimus.wasmer.app/';
+$dir=__DIR__;
+$sitemapFile='sitemap.xml';
+$allFile='all.html';
+$baseUrl='https://urimus.wasmer.app/';
 
-$exclude = [
-	'index.html',
-	'test.html',
-	'google5577016dee36085f.html'
-];
+function filenameToTitle($file){
+	$name=pathinfo($file,PATHINFO_FILENAME);
+	return ucfirst(str_replace(['-','_'],' ',$name));
+}
 
-$files = scandir($dir);
+$exclude=['index.html','test.html','google5577016dee36085f.html'];
 
-$xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>');
+$files=scandir($dir);
+$htmlLinks=[];
 
-$indexFile = $dir . '/index.html';
-$lastmodMain = file_exists($indexFile)
-	? date('Y-m-d', filemtime($indexFile))
-	: date('Y-m-d');
+$indexFile=$dir.'/index.html';
+$lastmodMain=file_exists($indexFile)?date('Y-m-d',filemtime($indexFile)):date('Y-m-d');
 
-$url = $xml->addChild('url');
-$url->addChild('loc', $baseUrl);
-$url->addChild('lastmod', $lastmodMain);
+$sitemapLines=[];
+$sitemapLines[]="<url><loc>{$baseUrl}</loc><lastmod>{$lastmodMain}</lastmod></url>";
+$sitemapLines[]="<url><loc>{$baseUrl}{$allFile}</loc><lastmod>".date('Y-m-d')."</lastmod></url>";
 
-$htmlLinks = [];
+foreach($files as $file){
+	if($file==='.'||$file==='..'||in_array(strtolower($file),[strtolower($sitemapFile)],true)) continue;
+	if(in_array($file,$exclude,true)) continue;
 
-foreach ($files as $file) {
-
-	if ($file === '.' || $file === '..' || $file === $sitemapFile) continue;
-	if (in_array($file, $exclude)) continue;
-
-	if (is_file($dir . '/' . $file) && strtolower(pathinfo($file, PATHINFO_EXTENSION)) === 'html') {
-
-		$loc = $baseUrl . $file;
-		if ($file === $allFile) {
-			$lastmod = date('Y-m-d');
-		} else {
-			$lastmod = date('Y-m-d', filemtime($dir . '/' . $file));
-		}
-
-		$url = $xml->addChild('url');
-		$url->addChild('loc', $loc);
-		$url->addChild('lastmod', $lastmod);
-
-		if ($file !== $allFile) {
-			$htmlLinks[] = [
-				'file' => $file,
-				'url' => $loc
-			];
-		}
+	$path=$dir.'/'.$file;
+	if(is_file($path) && strtolower(pathinfo($file,PATHINFO_EXTENSION))==='html' && $file!==$allFile){
+		$loc=$baseUrl.$file;
+		$lastmod=date('Y-m-d',filemtime($path));
+		$sitemapLines[]="<url><loc>{$loc}</loc><lastmod>{$lastmod}</lastmod></url>";
+		$htmlLinks[]=['title'=>filenameToTitle($file),'url'=>$loc];
 	}
 }
 
-$allExists = file_exists($dir . '/' . $allFile);
+usort($htmlLinks,function($a,$b){return strcmp($a['title'],$b['title']);});
 
-$allHtml = "<!DOCTYPE html>
-<html lang='en'>
-<head>
-	<meta charset='UTF-8'>
-	<title>All Pages</title>
-</head>
-<body>
-	<h1>All Pages</h1>
-	<ul>
-	<li><a href=\"{$baseUrl}\">Home</a></li>";
-
-foreach ($htmlLinks as $page) {
-	$allHtml .= "<li><a href=\"{$page['url']}\">{$page['file']}</a></li>";
+$allHtml="<html lang='en'><head><meta charset='UTF-8'><title>Site Map</title></head><body><h1>Site Map</h1><ul><li><a href=\"{$baseUrl}\">Home</a></li>";
+foreach($htmlLinks as $page){
+	$title=htmlspecialchars($page['title']);
+	$url=htmlspecialchars($page['url']);
+	$allHtml.="<li><a href=\"{$url}\">{$title}</a></li>";
 }
+$allHtml.="</ul></body></html>";
 
-$allHtml .= "</ul>
-</body>
-</html>";
+file_put_contents($dir.'/'.$allFile,$allHtml);
 
-file_put_contents($dir . '/' . $allFile, $allHtml);
+$sitemapXml="<?xml version=\"1.0\" encoding=\"UTF-8\"?><urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">".implode('',$sitemapLines)."</urlset>";
 
-if (!$allExists) {
-	$url = $xml->addChild('url');
-	$url->addChild('loc', $baseUrl . $allFile);
-	$url->addChild('lastmod', date('Y-m-d'));
-}
-
-$dom = new DOMDocument('1.0', 'UTF-8');
-$dom->preserveWhiteSpace = false;
-$dom->formatOutput = true;
-$dom->loadXML($xml->asXML());
-
-file_put_contents($dir . '/' . $sitemapFile, $dom->saveXML());
+file_put_contents($dir.'/'.$sitemapFile,$sitemapXml);
 
 echo '<script>alert("Sitemap and all.html generated");</script>';
