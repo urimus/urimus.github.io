3fd02ceac93cdf0753e7d18697605156d5695dd3
Urmas Repinski <urimus@yahoo.com>
2026-03-09 03:59:38 +0200

    all.html added

--- scripts/proxy/generate_sitemap.php
+++ scripts/proxy/generate_sitemap.php
@@ -1,6 +1,7 @@
 <?php
 $dir = __DIR__;
 $sitemapFile = 'sitemap.xml';
+$allFile = 'all.html';
 $baseUrl = 'https://urimus.wasmer.app/';
 
 $exclude = [
@@ -22,16 +23,55 @@ $url = $xml->addChild('url');
 $url->addChild('loc', $baseUrl);
 $url->addChild('lastmod', $lastmodMain);
 
+$htmlLinks = [];
+
 foreach ($files as $file) {
+
 	if ($file === '.' || $file === '..' || $file === $sitemapFile) continue;
 	if (in_array($file, $exclude)) continue;
 
 	if (is_file($dir . '/' . $file) && strtolower(pathinfo($file, PATHINFO_EXTENSION)) === 'html') {
+
+		$loc = $baseUrl . $file;
+		$lastmod = date('Y-m-d', filemtime($dir . '/' . $file));
+
 		$url = $xml->addChild('url');
-		$url->addChild('loc', $baseUrl . $file);
-		$url->addChild('lastmod', date('Y-m-d', filemtime($dir . '/' . $file)));
+		$url->addChild('loc', $loc);
+		$url->addChild('lastmod', $lastmod);
+
+		if ($file !== $allFile) {
+			$htmlLinks[] = [
+				'file' => $file,
+				'url' => $loc
+			];
+		}
 	}
 }
+
+$allHtml = "<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<meta charset='UTF-8'>
+	<title>All Pages</title>
+</head>
+<body>
+	<h1>All Pages</h1>
+	<ul>
+	<li><a href=\"{$baseUrl}\">Home</a></li>";
+
+foreach ($htmlLinks as $page) {
+	$allHtml .= "<li><a href=\"{$page['url']}\">{$page['file']}</a></li>";
+}
+
+$allHtml .= "</ul>
+</body>
+</html>";
+
+file_put_contents($dir . '/' . $allFile, $allHtml);
+
+$url = $xml->addChild('url');
+$url->addChild('loc', $baseUrl . $allFile);
+$url->addChild('lastmod', date('Y-m-d'));
 
 $dom = new DOMDocument('1.0', 'UTF-8');
 $dom->preserveWhiteSpace = false;
@@ -40,5 +80,4 @@ $dom->loadXML($xml->asXML());
 
 file_put_contents($dir . '/' . $sitemapFile, $dom->saveXML());
 
-echo '<script>alert("Sitemap сгенерирован - ' . $dir . '/' . $sitemapFile . '");</script>';
-
+echo '<script>alert("Sitemap and all.html generated");</script>';
