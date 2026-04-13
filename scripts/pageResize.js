b62d21275ef842c854c12acc27e434ebaa29ed97
Urmas Repinski <urimus@yahoo.com>
2026-04-13 05:02:26 +0300

    Update pageResize.js

--- scripts/pageResize.js
+++ scripts/pageResize.js
@@ -8,7 +8,7 @@ var initComplete = false;
 
 // --- service worker ---
 if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register("/serviceWorker.js")
+	navigator.serviceWorker.register("/serviceWorker.js", { scope: "/" })
 	.then(function (reg) {
 		serviceWorkerStarted = true;
 	});
