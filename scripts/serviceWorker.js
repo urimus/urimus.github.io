8c8e9b13332bf923c76f8eb95f89b9f5d09b192e
Urmas Repinski <urimus@yahoo.com>
2026-04-13 04:31:39 +0300

    Update serviceWorker.js

--- scripts/serviceWorker.js
+++ scripts/serviceWorker.js
@@ -31,8 +31,20 @@ self.addEventListener("message", function (event) {
 	}
 });
 
+function log(...args) {
+    self.clients.matchAll().then(clients => {
+        clients.forEach(client => {
+            client.postMessage({
+                type: "SW_LOG",
+                data: args
+            });
+        });
+    });
+}
+
 self.addEventListener("fetch", function (event) {
-    console.log("FETCH:", event.request.url, event.request.destination);
+console.log("FETCH:", event.request.url, event.request.destination);
+log("FETCH:", event.request.url, event.request.destination);
 	if (event.request.destination !== "image") return;
 
 	event.respondWith(
@@ -44,10 +56,12 @@ self.addEventListener("fetch", function (event) {
 						if (IS_PRELOAD_MODE && fresh) {
 							// ✅ свежая → только кэш
 console.log("✅ свежая → только кэш");
+log("✅ свежая → только кэш");
 							return cached;
 						}
 
 console.log("⚠️ старая → вернуть + обновить в фоне");
+log("⚠️ старая → вернуть + обновить в фоне");
 						// ⚠️ старая → вернуть + обновить в фоне
 						event.waitUntil(
 							updateInBackground(cache, event.request)
@@ -58,6 +72,7 @@ console.log("⚠️ старая → вернуть + обновить в фон
 				}
 
 console.log("❌ нет в кэше → сеть + запись");
+log("❌ нет в кэше → сеть + запись");
 				// ❌ нет в кэше → сеть + запись
 				return fetchAndCache(cache, event.request);
 			});
