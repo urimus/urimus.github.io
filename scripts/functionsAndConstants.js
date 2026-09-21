f7f0b7ec4326a509e4b4705e520c03d066e801ea
Urmas Repinski <urimus@yahoo.com>
2026-09-21 17:47:02 +0300

    Update functionsAndConstants.js

--- scripts/functionsAndConstants.js
+++ scripts/functionsAndConstants.js
@@ -334,7 +334,6 @@ function getWordsCount(pointers, pointerTops, linesToShow, lineHeight, current, 
 	let wordsCount = 1;
 	let wordsCountM1 = 1;
 	let previousTop = tops[1];
-	let isBreak = false;
 
 	for (let i = 2; i < tops.length; i++) {
 		const top = tops[i];
@@ -343,8 +342,8 @@ function getWordsCount(pointers, pointerTops, linesToShow, lineHeight, current, 
 			previousTop = top;
 		}
 		if (linesCount > linesToShow) {
-			isBreak = true;
-			break;
+			if (expansionA) wordsCount = i - 1;
+			return { wordsCount, wordsCountM1 };
 		}
 		wordsCount = i;
 		if (linesCount <= linesToShow - 1) {
@@ -352,26 +351,18 @@ function getWordsCount(pointers, pointerTops, linesToShow, lineHeight, current, 
 		}
 	}
 	if (expansionA) {
-		if (isBreak) {
-			// The current word does not fit.
-			wordsCount = current - 1;
+		// All current words fit.
+		// Check whether the expansion link moves to the next line.
+		if (expansionTop > previousTop) {
+			linesCount += Math.max(1, Math.round((expansionTop - previousTop) / lineHeight));
+		}
+		if (linesCount <= linesToShow) {
+			wordsCount = current;
+			if (linesCount <= linesToShow - 1) {
+				wordsCountM1 = current;
+			}
 		} else {
-			// The current words fit.
-			// Check whether the expansion link moves to the next line.
-			if (expansionTop > previousTop) {
-				linesCount += Math.max(1, Math.round((expansionTop - previousTop) / lineHeight));
-			}
-			if (linesCount <= linesToShow) {
-				// The expansion link also fits.
-				wordsCount = current;
-				// There is still one full line of spare space.
-				if (linesCount <= linesToShow - 1) {
-					wordsCountM1 = current;
-				}
-			} else {
-				// The expansion link does not fit.
-				wordsCount = current - 1;
-			}
+			wordsCount = current - 1;
 		}
 	}
 	return { wordsCount, wordsCountM1 };
