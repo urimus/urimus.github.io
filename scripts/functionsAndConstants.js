62fe59801150002fea109f7eb347c07d5fb7ebfc
Urmas Repinski <urimus@yahoo.com>
2026-09-22 19:22:36 +0300

    Update functionsAndConstants.js

--- scripts/functionsAndConstants.js
+++ scripts/functionsAndConstants.js
@@ -302,6 +302,7 @@ function modifySummary(element, words_arr, col = "blue", linesToShow = 4) {
 console.log(words_arr);
 console.log("expansionA.offsetTop -",expansionA.offsetTop);
 console.log("lastLineTop -",lastLineTop);
+console.log("wordsCount -",wordsCount);
 	if (Math.abs(expansionA.offsetTop - lastLineTop) >= 2) {
 		while (wordsCount > 1) {
 			wordsCount--;
